a64ddd218e9ddb1becde9cf5e8c4a25cecadf10b
Nishan <nishancse@gmail.com>
2021-08-18 23:16:31 +0600

    added initial commit

---
+++ main.py
@@ -0,0 +1,46 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import pickle
+
+app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
+
+#Write APIS
+@app.get('/')
+async def root():
+    return {'hello' : 'world'}
+
+#To access api doc
+# {base-url}/docs
+
+#Overall rank score list
+@app.get('/overall-ranks')
+async def root():
+    pickle_in = open("./resources/over_all_ranking_list.pickle","rb")
+    over_all_ranking_list = pickle.load(pickle_in)
+    pickle_in.close()
+    return over_all_ranking_list
+
+#API of experience of years ranking
+@app.get('/experience-ranks')
+async def root():
+    pickle_in = open("./resources/years_of_experience_list.pickle","rb")
+    years_of_experience_list = pickle.load(pickle_in)
+    pickle_in.close()
+    return years_of_experience_list
+
+#API of experience of years ranking
+@app.get('/requirement-matching-scores')
+async def root():
+    pickle_in = open("./resources/similarity_socre_list.pickle","rb")
+    similarity_socre_list = pickle.load(pickle_in)
+    pickle_in.close()
+    return similarity_socre_list
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Following Commands has been executed to run the project successfully
+sudo pip3 install pipenv
+pipenv shell
+pipenv install fastapi
+pipenv install uvicorn
+sudo pip3 install numpy
+
+#To start app
+unicorn main:app --reload
