0a5f4c8c1297ef6346da520a82670b76278fd1be
Nishan <nishancse@gmail.com>
2021-08-19 00:50:12 +0600

    removed cors filter

--- main.py
+++ main.py
@@ -1,17 +1,8 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 import pickle
 
 app = FastAPI()
 
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=['*'],
-    allow_credentials=True,
-    allow_methods=['*'],
-    allow_headers=['*'],
-)
 
 #Write APIS
 @app.get('/')
